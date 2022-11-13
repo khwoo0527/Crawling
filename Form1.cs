b32bed770588b9a6f8df0bcd2d9f797bd53dbf92
현우 김 <khwoo0527@gmail.com>
2022-11-13 23:23:04 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrawlingPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            System.Console.WriteLine("test");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("커밋하고 푸쉬가 되고 있는거 맞어?? 깃이랑 깃허브랑은 좀 다른가?");
+            // 잘 되넹 그냥 깃이랑은 초큼 다르네엥 회사컴이랑 옵션이 달라서 그런건가??!! 뚜둥
+        }
+    }
+}
